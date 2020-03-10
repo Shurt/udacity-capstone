f68fe06d812dfda2e57886049497bd163a808343
Kyle Kuypers <shurt123@gmail.com>
2020-03-10 14:16:52 -0500

    One more time

---
+++ database/models.py
@@ -0,0 +1,102 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+database_path = os.getenv('DATABASE_URL')
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename
+    variable to have multiple verisons of a database
+'''
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+# Movies with attributes title and release date
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = Column(db.Integer, primary_key=True)
+    title = Column(String(80), unique=True)
+    release_date = Column(String(180), nullable=False)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+# Actors with attributes name, age and gender
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+    id = Column(db.Integer, primary_key=True, autoincrement=True)
+    name = Column(String(80), unique=True)
+    age = Column(String(180), nullable=False)
+    gender = Column(String(80), nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
